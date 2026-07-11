11344e68c4d05da07afabf3a15f9d310e02589f3
phongluong8901 <phongluong3366@gmail.com>
2026-07-11 10:46:26 +0700

    change: add backend Apis

--- backend_api/index.js
+++ backend_api/index.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const authRouter = require('./routes/auth');
+const bannerRouter = require('./routes/banner')
+const categoryRouter = require('./routes/category');
+const subcategoryRouter = require('./routes/sub_category');
+const productRouter = require('./routes/product');
+const productReviewRouter = require('./routes/product_review');
 
 const PORT = 3000; //defind port number the server will listen on
 
@@ -22,6 +27,11 @@ app.use(cors({
 //middleware - toregister routes or to mount routes
 app.use(express.json());
 app.use(authRouter);
+app.use(bannerRouter);
+app.use(categoryRouter);
+app.use(subcategoryRouter);
+app.use(productRouter);
+app.use(productReviewRouter);
 
 mongoose.connect(DB).then(()=> {
     console.log('mongodb Connected')
---
+++ backend_api/routes/category.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const Category = require('../models/category');
+
+const categoryRouter = express.Router();
+
+categoryRouter.post('/api/categories', async(req, res) => {
+    try {
+        const {name, image, banner} = req.body;
+        const category = new Category({name, image, banner});
+        await category.save();
+        res.status(201).send(category);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+categoryRouter.get('/api/categories', async(req, res) => {
+    try {
+        const categories = await Category.find();
+        res.status(200).send({categories});
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+})
+
+module.exports = categoryRouter;
